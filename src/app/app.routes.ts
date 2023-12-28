0c248a68e5e3e6ad948c31b070cd61566927bfee
LucasRomero <romerolucasdaniel@outlook.com>
2023-12-29 12:21:23 +1300

    -add rules schematics -add tailwind css -first route for login -router-outlet in component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./login/login.component').then((a) => a.LoginComponent),
+  },
+];
